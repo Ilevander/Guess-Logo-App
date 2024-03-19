eb615fdcb6394af94ed7c1e13923bcd4be76a509
dell <dell@DESKTOP-4P8U8AR>
2024-03-20 00:40:39 +0100

    Creating Question Class

---
+++ app/src/main/java/com/example/guess_logo/Questions.java
@@ -0,0 +1,92 @@
+package com.example.guess_logo;
+
+public class Questions {
+    private String mQuestions[] ={
+            "Guess the Logo ?",
+            "You Can Guess the Logo ?",
+            "Can you this one?",
+            "Guess the Logo ?",
+            "Guess the Logo ?",
+            "Can you this one?",
+            "Guess the Logo ?",
+            "Guess the Logo ?",
+            "Can you this one?",
+            "Guess the Logo ?",
+            "Guess the Logo ?",
+    };
+    
+    private String mChoice[] [] = {
+            {"Android","IOS","Both"},
+            {"Mozilla","Chrome","Brave"},
+            {"Hp","Dell","ThinKPad"},
+            {"GitLab","Github","SourceForge "},
+            {"Flutter","React-Native","Xamarin"},
+            {"Yahoo Mail","Gmail","Zoho Mail"},
+            {"BackBox","Security Onion","Kali-Linux"},
+            {"NVIDIA Corporation","Intel","IBM Corporation"},
+            {"C#","JavaScript","Java"},
+            {"Kotlin","Dart","Swift"},
+            {"Outlook","Other","Gmail"},
+            {"Xiaomi","OPPO","Oraimo"},
+            {"CryEngine","Unity","Amazon Lumberyard"},
+    };
+    
+    private String mImages[] = {
+            "q1", //Android image
+            "q2", //Chrome image
+            "q3", //Dell image
+            "q5", //Github image
+            "q6", //Flutter image
+            "q7", //Gmail image
+            "q8", //Kali Linux image
+            "q9", //Intel image
+            "q10", //Java image
+            "q11", //Kotlin image
+            "q12", //Microsoft image
+            "q13", //Nvidia image
+            "q14", //Opera image
+            "q15", //Outlook image
+            "mic", //Xiaomi image
+            "unt", //Unity
+    };
+
+    private String mQuestionsType[] = {
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+            "radiobutton",
+    };
+
+    private String mCorrecrtAnswers[] = {
+            "Android",
+            "Chrome",
+            "Dell",
+            "Github",
+            "Flutter",
+            "Gmail",
+            "Kali Linux",
+            "Intel",
+            "Java",
+            "Kotlin",
+            "Microsoft",
+            "Nvidia",
+            "Opera",
+            "Outlook",
+            "Xiaomi",
+            "Unity",
+    };
+
+
+}
