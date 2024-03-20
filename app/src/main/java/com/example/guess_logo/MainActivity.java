081abddcc5ff75e4668a047359d13a74fc7a8fee
dell <dell@DESKTOP-4P8U8AR>
2024-03-20 01:37:49 +0100

    Fetching View inside the Main Activity

--- app/src/main/java/com/example/guess_logo/MainActivity.java
+++ app/src/main/java/com/example/guess_logo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.guess_logo;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,24 +12,37 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView questionTextView;
     private Button submitButton;
-
+    private ImageView mQuizImage;
+    private int mScore = 0;
+    private int mQuizNum = 1;
+    private int QuestionNum = 0;
+    private TextView mQuestionView;
+    private TextView mQuizNumView;
+    private Questions mQuestions = new Questions();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find views by their IDs
-        questionTextView = findViewById(R.id.question_textview);
-        submitButton = findViewById(R.id.button_submit);
+       mQuestionView = findViewById(R.id.question_textview);
+       mQuizNumView = findViewById(R.id.quiznum_textview);
 
-        // Set click listener for the submit button
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle submit button click
-                String question = questionTextView.getText().toString();
-                // Process the question or perform any other action
-            }
-        });
+       Button submit = findViewById(R.id.button_submit);
+       submit.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View view){
+             //Quiz Logic
+           }
+       });
     }
+    @SuppressLint("DiscouragedApi")
+    private void showMainImage(){
+        mQuizImage = findViewById(R.id.quiz_image);
+        String img = mQuestions.getmImages(QuestionNum);
+        mQuizImage.setImageResource(getResources().getIdentifier(img,"drawable",getPackageName()));
+
+    }
+
+
+
 }
